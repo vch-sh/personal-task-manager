dce8e5eca1b2741d03ebf9f46a361e6f09f9538f
Viacheslav <slavo@grownapps.io>
2024-11-21 16:30:40 +0200

    Implemented authorization with Github

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,8 @@
+import { auth } from '@/auth';
+
 export default async function DashboardPage() {
+  const session = await auth();
+  console.log('🚀 ~ DashboardPage ~ session:', session);
+
   return <div>DashboardPage</div>;
 }
