4c1677bdc4873feb336a7105fbc59d65a8799e44
Viacheslav <slavo@grownapps.io>
2024-11-28 15:53:45 +0200

    Dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,32 @@
 import { auth } from '@/auth';
-import SignOutBtn from './SignOutBtn';
+import { getDashboardData } from '@/app/data/getDashboardData';
+import Header from '@/components/DashboardPage/Header';
+import TaskStatistics from '@/components/DashboardPage/TaskStatistics';
+import { getTasks } from '@/actions/GetTasks';
+import Task from '@/types/Task';
 
 export default async function DashboardPage() {
   const session = await auth();
-  console.log('🚀 ~ DashboardPage ~ session:', session);
+  const tasks: Task[] = await getTasks(session?.user.id);
+  const { completedTasks, inProgressTasks, todoTasks } =
+    getDashboardData(tasks);
 
   return (
-    <div>
-      DashboardPage <SignOutBtn />
-    </div>
+    <main className="container mx-auto p-4 h-screen max-w-5xl min-w-[360px]">
+      <Header />
+
+      {tasks.length > 0 ? (
+        <TaskStatistics
+          tasksQuantity={tasks.length}
+          completedTasksQuantity={completedTasks.length}
+          inProgressTasksQuantity={inProgressTasks.length}
+          todoTasksQuantity={todoTasks.length}
+        />
+      ) : (
+        <p className="text-default text-sm text-center font-semibold">
+          Add your first task to get started with statistics
+        </p>
+      )}
+    </main>
   );
 }
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,2 +1,8 @@
+import Task from '@/types/Task';
+
 export const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/g;
 export const passwordRegex = /[!@#$%]+/;
+
+export function getTasksByStatus(tasks: Task[], value: string) {
+  return tasks.filter((task: Task) => task.status === value);
+}
