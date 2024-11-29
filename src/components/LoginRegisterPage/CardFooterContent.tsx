6bae22598c56782badc725f3f7284cca94a4674e
Viacheslav <slavo@grownapps.io>
2024-11-29 15:21:38 +0200

    Login page footer positioning fix.

--- src/components/LoginRegisterPage/CardFooterContent.tsx
+++ src/components/LoginRegisterPage/CardFooterContent.tsx
@@ -3,7 +3,8 @@ import Link from 'next/link';
 export default function CardFooterContent() {
   return (
     <p className="text-xs text-center text-default w-full">
-      By clicking continue, you agree to our{' '}
+      By clicking continue, you agree to our
+      <br />
       <Link
         href="/terms"
         className="underline underline-offset-4 cursor-not-allowed"
