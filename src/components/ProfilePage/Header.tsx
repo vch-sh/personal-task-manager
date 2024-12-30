cd0990b9befa191ea32e789bf5724ee1356e3c26
Viacheslav <slavo@grownapps.io>
2024-12-30 09:37:35 +0200

    Minor UI update

--- src/components/ProfilePage/Header.tsx
+++ src/components/ProfilePage/Header.tsx
@@ -4,21 +4,16 @@ import { Button } from '@/components/ui/button';
 
 export default function Header() {
   return (
-    <>
-      <header className="flex items-center justify-between">
-        <h2 className="text-2xl font-semibold tracking-tight sm:text-3xl">
-          Profile Settings
-        </h2>
-        <Link href="/tasks">
-          <Button className="transition-all active:scale-90">
-            <MoveLeft />
-            Tasks
-          </Button>
-        </Link>
-      </header>
-      <p className="mt-4 text-sm font-bold text-red-500">
-        Frontend part available. Backend functionality is still in development.
-      </p>
-    </>
+    <header className="flex items-center justify-between">
+      <h2 className="text-2xl font-semibold tracking-tight sm:text-3xl">
+        Profile Settings
+      </h2>
+      <Link href="/tasks">
+        <Button className="transition-all active:scale-90">
+          <MoveLeft />
+          Tasks
+        </Button>
+      </Link>
+    </header>
   );
 }
